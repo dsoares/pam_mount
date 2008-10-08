ebdf10952580bca64c5a51a8b0c6713ba6ac958e
Jan Engelhardt <jengelh@medozas.de>
2008-10-08 09:37:26 -0400

    src: fix return value of spawn_synchronous

--- src/spawn.c
+++ src/spawn.c
@@ -103,18 +103,22 @@ static bool __spawn_start(const char *const *argv, pid_t *pid, int *fd_stdin,
     const char *user)
 {
 	const int *fd_rq[] = {fd_stdin, fd_stdout, fd_stderr};
-	int pipes[3][2], ret;
+	int pipes[3][2], ret, saved_errno;
 
 	if ((ret = spawn_build_pipes(fd_rq, pipes)) < 0) {
+		saved_errno = -ret;
 		l0g("pipe(): %s\n", strerror(-ret));
+		errno = ret;
 		return false;
 	}
 
 	spawn_set_sigchld();
 	if ((*pid = fork()) < 0) {
+		saved_errno = errno;
 		l0g("fork(): %s\n", strerror(errno));
 		spawn_restore_sigchld();
 		spawn_close_pipes(pipes);
+		errno = saved_errno;
 		return false;
 	} else if (*pid == 0) {
 		if (setup != NULL)
@@ -173,7 +177,7 @@ int spawn_synchronous(const char *const *argv)
 	int ret;
 
 	if (!__spawn_start(argv, &pid, NULL, NULL, NULL, NULL, NULL))
-		return false;
+		return -errno;
 	waitpid(pid, &ret, 0);
 	spawn_restore_sigchld();
 	return ret;
