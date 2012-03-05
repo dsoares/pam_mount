fa2422d34e69ada5d29f1fe0df6cc86c446aa228
Jan Engelhardt <jengelh@medozas.de>
2012-03-05 12:13:01 +0100

    pam_mount: understand crypt_LUKS and crypto_LUKS in xml file
    Just like with "crypt", do not attempt to decode the SSL key in
    pam_mount.so if these two extra filesystem types have been used in
    <volume>.

--- src/rdconf1.c
+++ src/rdconf1.c
@@ -1356,6 +1356,10 @@ static const char *rc_volume(xmlNode *node, struct config *config,
 }
 
 //-----------------------------------------------------------------------------
+/*
+ * This list has no linear relation to vpt->command. Instead, what is done:
+ *   config->command[default_command[i].type] = <command>
+ */
 static const struct pmt_command default_command[] = {
 	{CMD_SMBMOUNT,   "smbfs", {"smbmount", "%(COMBOPATH)", "%(MNTPT)", "-o", "username=%(USER),uid=%(USERUID),gid=%(USERGID)%(if %(OPTIONS),\",%(OPTIONS)\")", NULL}},
 	{CMD_SMBUMOUNT,  "smbfs", {"smbumount", "%(MNTPT)", NULL}},
@@ -1369,6 +1373,8 @@ static const struct pmt_command default_command[] = {
 	{CMD_NFSMOUNT,    "nfs4"},
 	{CMD_LCLMOUNT,    NULL,    {"mount", "-p0", "%(if %(OPTIONS),-o%(OPTIONS))", "-t%(FSTYPE)", "%(VOLUME)", "%(MNTPT)", NULL}},
 	{CMD_CRYPTMOUNT,  "crypt", {"mount", "-t", "crypt", "%(if %(CIPHER),-ocipher=%(CIPHER))", "%(if %(FSKEYCIPHER),-ofsk_cipher=%(FSKEYCIPHER))", "%(if %(FSKEYHASH),-ofsk_hash=%(FSKEYHASH))", "%(if %(FSKEYPATH),-okeyfile=%(FSKEYPATH))", "%(if %(OPTIONS),-o%(OPTIONS))", "%(VOLUME)", "%(MNTPT)", NULL}},
+	{CMD_CRYPTMOUNT,  "crypt_LUKS"},
+	{CMD_CRYPTMOUNT,  "crypto_LUKS"},
 	{CMD_CRYPTUMOUNT, "crypt", {"umount.crypt", "%(MNTPT)", NULL}},
 	{CMD_UMOUNT,     NULL,     {"umount", "%(MNTPT)", NULL}},
 	{CMD_FSCK,       NULL,     {"fsck", "-p", "%(FSCKTARGET)", NULL}},
